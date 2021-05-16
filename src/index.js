4222eb04e9a888edd6b2102f48ecab4166aa55c6
Rost-Perig <rost.perig@gmail.com>
2021-05-16 11:28:00 +0300

    create header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './sass/main.scss';
+//скрипт для меню хедера
+(() => {
+  const openMenuBtnRef = document.querySelector("[data-open-menu-button]");
+  const closeMenuBtnRef = document.querySelector("[data-close-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+
+  openMenuBtnRef.addEventListener("click", () => {
+    mobileMenuRef.classList.add("menu-is-open");
+    mobileMenuRef.setAttribute("aria-expanded", true);
+    openMenuBtnRef.setAttribute("aria-expanded", false);
+  });
+
+  closeMenuBtnRef.addEventListener("click", () => {
+    mobileMenuRef.classList.remove("menu-is-open");
+    mobileMenuRef.setAttribute("aria-expanded", false);
+    closeMenuBtnRef.setAttribute("aria-expanded", true);
+  });
+})();
