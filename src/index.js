f850e77120f0ccca37a97329231c4fa0ec71674e
Smolldo <maximqwerbich@gmail.com>
2021-05-16 18:24:57 +0300

    адаптировання карусель

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './sass/main.scss';
+
+$(document).ready(function(){
+    $('.slider').slick({
+        centerMode: true,
+        centerPadding: '10px',
+        slidesToShow:3,
+        slideToScroll:1,
+        arrows:true,
+        responsive: [
+        {
+            breakpoint:768,
+            settings:{
+                slidesToShow:1
+            }
+        }
+        ]
+    
+    });
+});
