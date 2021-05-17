e43e913c23633aa33e53cf71695969d16769a8c1
Smolldo <maximqwerbich@gmail.com>
2021-05-17 14:49:04 +0300

    адаптированная версия карусели

--- src/slider.js
+++ src/slider.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
     $('.slider').slick({
-        centerMode: true,
-        centerPadding: '10px',
-        slidesToShow:3,
-        slideToScroll:1,
+    centerMode: true,
+    centerPadding:'10px',
+    slidesToShow: 3,
+    slidesToScroll: 1,
         arrows:true,
         responsive: [
         {
             breakpoint:768,
             settings:{
-                slidesToShow:1
+                slidesToShow:1,
+                arrows:false
             }
         }
         ]
