8d8d53ac14267b0dca7cbf6c7087ef012c66825e
Smolldo <maximqwerbich@gmail.com>
2021-05-21 11:39:06 +0300

    Add scripts
    Добавлен скрипт на плавный скролл, а так же добавлена кнопка backToTop

--- src/slider.js
+++ src/slider.js
@@ -40,7 +40,33 @@ if ($slider.length) {
         }
         ]
     
+        
     });
     
+    $(".site-menu").on("click","a", function (event) {
+      event.preventDefault();
+      var id  = $(this).attr('href'),
+          top = $(id).offset().top;
+      $('body,html').animate({scrollTop: top}, 300)}); 
+
+
+      $(window).scroll(function() {
+ 
+        if($(this).scrollTop() != 0) {
+         
+        $('.toTop').fadeIn();
+         
+        } else {
+         
+        $('.toTop').fadeOut();
+         
+        }
+         
+        });
+         
+        $('.toTop').click(function() {
+         
+        $('body,html').animate({scrollTop:0},400)});
+        
 });
 
